96eb76c7049e28a6a5eabb489f4f555f7342be49
Jeesoong <jeesoong@live.com>
2016-09-10 11:42:30 +0800

    test

--- build/git.js
+++ build/git.js
@@ -11,56 +11,85 @@ const colors = require('colors')
 
 let currentBranch = 0
 
-let command = process.argv[2]
-
-switch (command) {
-    case 'newBranch':
-        newBranch()
-        break
-    default:
-        console.log(11)
-}
 
 
-function newBranch () {
-    exec('git branch -a & git tag', function (err, stdout, stderr) {
-        var r = clamUtil.getBiggestVersion(stdout.match(/\d+\.\d+\.\d+/ig));
-        if (!r) {
-            r = '0.1.0';
-        } else {
-            r = updateVersion(r)
-        }
-        console.log(colors.green('新分支：daily/' + r))
-        currentBranch = r
-        exec(`git checkout -b daily/${currentBranch}`, {async:true})
-        // 回写入 abc.json 的 version
-        let abcFile = path.resolve(process.cwd(), 'abc.json')
-        let abcJSON = require(abcFile)
-        abcJSON.version = r
-        fs.writeFile(abcFile, JSON.stringify(abcJSON), (err) => {
-            if (err) console.log(colors.red(err))
-            console.log(colors.green("update abc.json."))
-            exec('git add .', {async:true})
-            exec('git commit -m "test"', {async:true})
-            exec(`git push origin daily/${currentBranch}`, {async:true})
-        })
 
-    })
-}
+class gitWorkFlow {
+    constructor () {
+        this.init()
+    }
 
-function updateVersion (versionArr) {
-    if (versionArr[2] < 9) {
-        versionArr[2]++
-    } else {
-        versionArr[2] = 0
-        if (versionArr[1] < 9) {
-            versionArr[1]++
-        } else {
-            versionArr[1] = 0
-            versionArr[0]++
+    init () {
+        let command = process.argv[2]
+
+        switch (command) {
+            case 'newBranch':
+                this.newBranch()
+                break
+            default:
+                console.log(11)
         }
     }
-    return versionArr.join('.')
+
+    // 创建新的分支
+    newBranch () {
+        let me = this
+        exec('git branch -a & git tag', function (err, stdout, stderr) {
+            var r = clamUtil.getBiggestVersion(stdout.match(/\d+\.\d+\.\d+/ig));
+            if (!r) {
+                r = '0.1.0';
+            } else {
+                r = me.updateVersion(r)
+            }
+            console.log(colors.green('新分支：daily/' + r))
+            currentBranch = r
+            exec(`git checkout -b daily/${currentBranch}`, {async:true})
+            // 回写入 abc.json 的 version
+            let abcFile = path.join(__dirname, 'abc.json')
+            let abcJSON = require(abcFile)
+            abcJSON.version = r
+            try {
+                fs.writeFile(abcFile, JSON.stringify(abcJSON), (err) => {
+                    if (err) console.log(colors.red(err))
+                    console.log(colors.green("update abc.json."))
+                    me.add()
+                    me.commit()
+                    me.push(currentBranch)
+                })
+            } catch (e) {
+                console.log(colors.red('未找到abc.json'))
+            }
+
+
+        })
+    }
+
+    add() {
+        exec('git add .', {async: true})
+    }
+
+    commit() {
+        exec('git commit -m "test"', {async: true})
+    }
+
+    push(currentBranch) {
+        exec(`git push origin daily/${currentBranch}`, {async: true})
+    }
+
+    updateVersion(versionArr) {
+        if (versionArr[2] < 9) {
+            versionArr[2]++
+        } else {
+            versionArr[2] = 0
+            if (versionArr[1] < 9) {
+                versionArr[1]++
+            } else {
+                versionArr[1] = 0
+                versionArr[0]++
+            }
+        }
+        return versionArr.join('.')
+    }
 }
 
-newBranch()
+new gitWorkFlow()
